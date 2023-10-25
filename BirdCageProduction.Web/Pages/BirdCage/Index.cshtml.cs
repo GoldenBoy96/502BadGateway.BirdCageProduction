727111093636c73f2f741be75e542bbbe5b18539
khanghuu <huukhanggl02@gmail.com>
2023-10-26 01:59:15 +0700

    update

---
+++ BirdCageProduction.Web/Pages/BirdCage/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BirdCageProduction.Web.Pages.BirdCage
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
