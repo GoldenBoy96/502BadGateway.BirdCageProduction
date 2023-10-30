e1345738fddd70062068e05deb367f3f2837aaf9
Cậu vàng <73738793+GoldenBoy96@users.noreply.github.com>
2023-10-30 17:05:27 +0700

    fix conflict BaseRepository.cs

--- Repository/Repositories/BaseRepository.cs
+++ Repository/Repositories/BaseRepository.cs
@@ -10,49 +10,48 @@ using System.Threading.Tasks;
 namespace Repository.Repositories
 {
     public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    protected readonly BirdCageProductionContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public BaseRepository(BirdCageProductionContext context)
     {
-        protected readonly BirdCageProductionContext _context;
-        private readonly DbSet<T> _dbSet;
-
-        public BaseRepository(BirdCageProductionContext context)
-        {
-            _context = context;
-            _dbSet = _context.Set<T>();
-        }
-
-        //public BirdCageProductionContext Context { get { return _context; } }
-
-        public async Task<bool> AddAsync(T entity)
-        {
-            await _dbSet.AddAsync(entity);
-            int success = await _context.SaveChangesAsync();
-
-            return await Task.FromResult(success == 1);
-        }
-
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            return await _dbSet.ToListAsync();
-        }
-
-        public async Task<T?> GetByIdAsync(int? id)
-        {
-            return await _dbSet.FindAsync(id);
-        }
-
-        public async Task<bool> RemoveAsync(T entity)
-        {
-            _dbSet.Remove(entity);
-            int success  =  await _context.SaveChangesAsync();
-            return await Task.FromResult(success == 1);
-        }
-
-        public async Task<bool> UpdateAsync(T entity)
-        {
-            _dbSet.Attach(entity);
-            _context.Entry(entity).State = EntityState.Modified;
-            int success = await _context.SaveChangesAsync();
-            return await Task.FromResult(success == 1);
-        }
+        _context = context;
+        _dbSet = _context.Set<T>();
     }
+
+
+    public async Task<bool> AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        int success = await _context.SaveChangesAsync();
+
+        return await Task.FromResult(success == 1);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int? id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<bool> RemoveAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        int success  =  await _context.SaveChangesAsync();
+        return await Task.FromResult(success == 1);
+    }
+
+    public async Task<bool> UpdateAsync(T entity)
+    {
+        _dbSet.Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+        int success = await _context.SaveChangesAsync();
+        return await Task.FromResult(success == 1);
+    }
+}
 }
