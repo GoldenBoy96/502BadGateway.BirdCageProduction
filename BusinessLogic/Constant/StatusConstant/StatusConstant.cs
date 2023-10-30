aee9d296c9cb333d9e7976d9458ec06d8eaa5f07
Cậu vàng <73738793+GoldenBoy96@users.noreply.github.com>
2023-10-30 17:01:22 +0700

    fix conflict StatusConstant.cs

--- BusinessLogic/Constant/StatusConstant/StatusConstant.cs
+++ BusinessLogic/Constant/StatusConstant/StatusConstant.cs
@@ -11,58 +11,46 @@ using System.Threading.Tasks;
 
 namespace BusinessLogic.Constant.StatusConstant
 {
-    public class StatusConstant : IStatusConstant
+public class StatusConstant : IStatusConstant
+{
+
+    private readonly DataAccess.BirdCageProductionContext _context;
+    IUnitOfWork _unitOfWork;
+
+    IAccountStatusRepository _accountStatusRepository;
+    CustomerStatusRepository _customerStatusRepository;
+    OrderStatusRepository _orderStatusRepository;
+    ProgressStatusRepository _progressStatusRepository;
+
+    List<AccountStatus> _accountStatusList = new();
+    List<string> _customerStatusList = new();
+    List<string> _orderStatusList = new();
+    List<string> _progressStatusList = new();
+
+
+    public StatusConstant(IUnitOfWork unitOfWork, IAccountStatusRepository accountStatusRepository, CustomerStatusRepository customerStatusRepository, OrderStatusRepository orderStatusRepository, ProgressStatusRepository progressStatusRepository)
     {
+        _unitOfWork = unitOfWork;
+        _accountStatusRepository = accountStatusRepository;
+        _customerStatusRepository = customerStatusRepository;
+        _orderStatusRepository = orderStatusRepository;
+        _progressStatusRepository = progressStatusRepository;
 
-        private readonly DataAccess.BirdCageProductionContext _context;
-        IUnitOfWork _unitOfWork;
+        _accountStatusList = _accountStatusRepository.GetAll().ToList();
 
-        AccountStatusRepository _accountStatusRepository;
-        //CustomerStatusRepository _customerStatusRepository;
-        //OrderStatusRepository _orderStatusRepository;
-        //ProgressStatusRepository _progressStatusRepository;
-
-        List<AccountStatus> _accountStatusList = new();
-        List<string> _customerStatusList = new();
-        List<string> _orderStatusList = new();
-        List<string> _progressStatusList = new();
-
-
-        //private static StatusConstant? _instance;
-
-        //public static StatusConstant Instance
-        //{
-        //    get
-        //    {
-        //        _instance ??= new StatusConstant();
-        //        return _instance;
-        //    }
-        //}
-
-        public StatusConstant(IUnitOfWork unitOfWork) {
-            _unitOfWork = unitOfWork;
-            //_accountStatusRepository = new(_context);
-            //_accountStatusList = GetAllAccountStatusAsync().Result;
-            //System.Diagnostics.Debug.WriteLine("================" + _accountStatusList[0].Name);
-            
-            _accountStatusList = _unitOfWork.AccountStatusRepository.GetAllAsync().Result.ToList(); 
-            
-        }
-
-        public async Task<List<AccountStatus>> GetAllAccountStatusAsync()
-        {
-            //_accountStatusList = (List<AccountStatus>)await _unitOfWork.AccountStatusRepository.GetAll();
-            return (List<AccountStatus>)await _accountStatusRepository.GetAllAsync();
-        }
-
-        public void PrintAccountStatus()
-        {
-            foreach (var accountStatus in _accountStatusList)
-            {
-                System.Diagnostics.Debug.WriteLine("================" + accountStatus.Name);
-            }
-        }
 
         
     }
+
+
+    public void PrintAccountStatus()
+    {
+        foreach (var accountStatus in _accountStatusList)
+        {
+            System.Diagnostics.Debug.WriteLine("================" + accountStatus.Name);
+        }
+    }
+
+    
+}
 }
