429fc81a9d9137a28cb3ee9b925397fecdc16e49
khanghuu <huukhanggl02@gmail.com>
2023-11-10 21:45:43 +0700

    procedure ui

--- BirdCageProduction.Web/Pages/ProcedurePage/Index.cshtml.cs
+++ BirdCageProduction.Web/Pages/ProcedurePage/Index.cshtml.cs
@@ -12,22 +12,10 @@ namespace BirdCageProduction.Web.Pages.ProcedurePage
 {
     public class IndexModel : PageModel
     {
-        private readonly DataAccess.BirdCageProductionContext _context;
-
-        public IndexModel(DataAccess.BirdCageProductionContext context)
-        {
-            _context = context;
-        }
-
-        public IList<Procedure> Procedure { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
-            if (_context.Procedures != null)
-            {
-                Procedure = await _context.Procedures
-                .Include(p => p.BirdCage).ToListAsync();
-            }
+
         }
     }
 }
